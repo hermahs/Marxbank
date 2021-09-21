b6fb5bbc5ece3733f08cc286cd98fbba078ff9a6
unknown <simeonc@stud.ntnu.no>
2021-09-21 09:36:59 +0200

    test(#17): oppdaterte tester for transaksjonsklasse

--- core/src/test/java/it1901/TransactionTest.java
+++ core/src/test/java/it1901/TransactionTest.java
@@ -3,28 +3,50 @@ package it1901;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class TransactionTest {
 
-    private Transaction transaction;    
+    private Transaction transaction;  
+    
+    private DataManager dm;
+    private User user1;
+    private User user2;
+    private Account a1;
+    private Account a2;
 
-    Account a1 = new SavingsAccount(new User(), 2);
-    Account a2 = new SavingsAccount(new User(), 3);
+    @TempDir
+    static Path tempDir;
+
+    @BeforeAll
+    public static void init() throws IOException {
+        Files.createDirectory(tempDir.resolve("data"));
+    }
 
     @BeforeEach
-    public void setup() {
+    public void beforeEach() throws IOException {
+        String path = tempDir.toFile().getCanonicalPath();
+        dm = new DataManager(path);
+        user1 = new User("id1", "username1", "email1@email.com", "password1", dm);
+        user2 = new User("id2", "username2", "email2@email.com", "password2", dm);
+        a1 = new SavingsAccount("id1", user1, 2, dm);
+        a2 = new SavingsAccount("id4", user2, 2, dm);
         a1.deposit(100);
         a2.deposit(100);
     }
-
     
     @Test
     @DisplayName("test commitTransaction with a1 and a2 as param")
     public void testCommitTransaction1() {
-        transaction = new Transaction(a1, a2, 50);
+        transaction = new Transaction("id1000", a1, a2, 50, true, dm);
 
         assertEquals(transaction.getFrom(), a1);
         assertEquals(transaction.getReciever(), a2);
@@ -41,7 +63,7 @@ public class TransactionTest {
     @DisplayName("test commitTransaction with a1 and null as param")
     public void testCommitTransaction2() {
         assertThrows(IllegalStateException.class, () -> {
-            transaction = new Transaction(a1, null, 50);;
+            transaction = new Transaction("id1001", a1, null, 50, true, dm);;
         });
 
         assertEquals(a1.getBalance(), 100);
